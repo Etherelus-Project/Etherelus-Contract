69f485530b03212d9a886273cb13545855011829
Florian CASTELAIN <castelainflorian44@gmail.com>
2018-02-12 20:53:26 +0100

    [WIP]

---
+++ contracts/decentralized_name_system/Decentralized-Name-System.sol
@@ -0,0 +1,47 @@
+// Written by Florian Castelain <castelainflorian44@gmail.com> for the Etherelus-Project
+// Version WIP
+pragma solidity ^0.4.18;
+contract DecentralizedNameSystem {
+
+    address public administrator;
+
+    struct System {
+        string value;
+        address owner;
+    }
+
+    mapping (string=>System) private domains;
+    uint256 private domainsCounter;
+
+    event AddSystem(string indexed key, string indexed value, address indexed sender);
+    event RemoveSystem(string indexed key, string indexed value, address indexed sender);
+
+    function DecentralizedNameSystem() public {
+        administrator = msg.sender;
+        domainsCounter = 0;
+    }
+
+    function addSystem(string key, string value, address owner) public {
+        AddSystem(key, value, owner);
+        System memory system = System(value, owner);
+        domains[key] = system;
+        domainsCounter++; 
+    }
+
+    modifier canDeleteSystem(string key) {
+        require((msg.sender == administrator) || (msg.sender == domains[key].owner));
+        _;
+    }
+
+    function removeSystem(string key) public canDeleteSystem {
+        RemoveSystem(key, domains[key].value, msg.sender);
+        delete domains[key];
+    }
+
+    function resolve(string key) public constant returns(string) {
+        return(domains[key].value);
+    }
+
+
+
+}
